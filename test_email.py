2618bad485aa41ad2209935189d477fa2659c0a9
rosmarcin <58086534+rosmarcin@users.noreply.github.com>
2021-05-05 14:08:09 +0200

    Add files via upload

---
+++ test_email.py
@@ -0,0 +1,11 @@
+import smtplib
+con=smtplib.SMTP('smtp.gmail.com',587)
+type(con)
+con.ehlo()
+#(250, b'smtp.gmail.com at your service, [37.30.61.161]\nSIZE 35882577\n8BITMIME\nSTARTTLS\nENHANCEDSTATUSCODES\nPIPELINING\nCHUNKING\nSMTPUTF8')
+con.starttls()
+# (220, b'2.0.0 Ready to start TLS')
+con.login('my@email.com','password')
+#(235, b'2.7.0 Accepted')
+con.sendmail('marcin.roszczyk@gmail.com','mr@marcinros.net','Subject: TEST')
+{}
